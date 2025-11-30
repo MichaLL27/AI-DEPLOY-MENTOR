7a568e01e024a90749365247ffe30025a63cf062
Michael Lazarashvili <50292926-lazarashvili270@users.noreply.replit.com>
2025-11-30 11:40:29 +0000

    Replit project main code

---
+++ tmp/android-builds/572b3239-9ad/android/app/src/main/java/com/aideploy/deployflowtest47oveu/MainActivity.java
@@ -0,0 +1,31 @@
+package com.aideploy.deployflowtest47oveu;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        WebView.setWebContentsDebuggingEnabled(true);
+        
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+
+        webView.loadUrl("https://deploy-flow-test-47oveu-572b3239.vercel.app");
+    }
+}
